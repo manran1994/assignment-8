ef62524bb12d707d74bb684bf43b25506a957ac5
manran1994 <43634837+manran1994@users.noreply.github.com>
2018-09-27 16:42:59 +0530

    Update ps2.py

--- ps2.py
+++ ps2.py
@@ -1,3 +1,27 @@
 ###### this is the second .py file ###########
 
 ####### write your code here ##########
+group = ['abcdefhi', 'jklmnopqr', 'stuvwxyz_']
+K = map(int, raw_input().split())
+A = raw_input()
+
+data = ['', '', '']
+ids = [[], [], []]
+for ii in range(len(A)): 
+    for gid in range(3):
+        if A[ii] in group[gid]: 
+            data[gid]+=A[ii]
+            ids[gid].append(ii)
+            break
+
+def rotate(gid):
+    k = K[gid] % len(ids[gid])
+    data[gid] = data[gid][-k:] + data[gid][:-k]
+
+res = ['x' for x in range(len(A))]
+for gid in range(3):
+    rotate(gid)
+    for ii in range(len(ids[gid])):
+        res[ids[gid][ii]] = data[gid][ii]
+
+print ''.join(res)
